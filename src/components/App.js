95284badcdf573e9d049eabeca8c97edaa51aa45
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-04-18 22:03:38 -0400

    nuevo shuffle

--- src/components/App.js
+++ src/components/App.js
@@ -4,29 +4,21 @@
 //
 import pokemon from '../data/pokemon/pokemon.js';
 let arrayImg =[];
-console.log("TEST");
-
 const App = () => {
   for (let i=0;i<pokemon.items.length;i++){
     arrayImg.push(pokemon.items[i].image);
   }
-  let div = document.createElement("div");
   arrayImg = arrayImg.reverse();
+  arrayImg.sort (()=> Math.random()-0.5);
+  
+  let div = document.createElement("div");
   console.log(arrayImg);
-  function shuffle (){
-    for (let i=0;i<arrayImg.length;i++){
-      let ranNumLoop = arrayImg[i];
-      let randomNum = Math.floor(Math.random() * (arrayImg.length-i))+i; 
-      arrayImg[i]= arrayImg[randomNum];// cambiar de valor posición num   
-      arrayImg[randomNum] = ranNumLoop; // reposicionar valor de posición random  
-      let el = document.createElement('img');
-      el.className = "imgShow";
-      el.src = arrayImg[i];
-      div.appendChild(el);
-    }
-    console.log(arrayImg);
-    }
-  shuffle(arrayImg);
+  for (let i=0;i<arrayImg.length;i++){ 
+    let el = document.createElement('img');
+    el.className = "visible";
+    el.src = arrayImg[i];
+    div.appendChild(el);
+  }
   return div;
 };
 

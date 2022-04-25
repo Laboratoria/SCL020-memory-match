e3988518896bdee18b64d7a24723e383d5e8f3d0
Iankat <ianka.ferrada@gmail.com>
2022-04-25 13:49:48 -0400

    Botones POP UP

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,16 @@ const verbssection = document.getElementById("verbssection");
 const buttonPlay = document.getElementById("play");
 const getUserName = document.getElementById("hi");
 const buttonColors = document.getElementById("buttonColors");
-let counter = 0;
-//let match = 8; 
-//let scoreText = document.querySelector("score");
 const buttonAnimals = document.getElementById("buttonAnimals");
 const buttonVerbs = document.getElementById("buttonVerbs");
 const time = document.getElementById("timer");
 let firstClick = false;
-
+let counter = 0;
+const congratulations = document.getElementById("congratulations");
+//const playAgain = document.getElementById("playAgain");
+const nextLevelAnimals = document.getElementById("nextLevelAnimals");
+const nextLevelVerbs = document.getElementById("nextLevelVerbs");
+const goToStart = document.getElementById("goToStart");
 
 //Botón jugar obtiene valor para próxima página y dirige a la siguiente página
 if (buttonPlay) {
@@ -57,150 +59,180 @@ if (buttonVerbs) {
 
 //Da tiempo entre volteo de cartas
 let isWaiting = false;
+function time() {
+  let secs = 0;
+  let mins = 0;
+  let SS
+  let MM
+  setInterval(() => {
+    secs++
+    if (secs == 60) {
+      secs = 0;
+      mins++
+    }
+    secs < 10 ? SS = `0${secs}` : SS = `${secs}` //recursividad
+    mins < 10 ? MM = `0${secs}` : SS = `${mins}`
+    document.getElementsByClassName("timer").innerHTML = `$(MM):$(SS)`
+  }, 1000);
+}
 
 
 const App = () => {
-  //Memory Colors
-  if (colorssection) {
-    const randomizedItems = colors.items.sort(() => Math.random() - 0.5);
-    for (const item of randomizedItems) {
-      const card = document.createElement("div");
-      const face = document.createElement("img");
-      const back = document.createElement("div");
-      card.classList.value = "card";
-      face.classList.value = "face";
-      back.classList.value = "backColors";
+    //Memory Colors
+    if (colorssection) {
+      const randomizedItems = colors.items.sort(() => Math.random() - 0.5);
+      for (const item of randomizedItems) {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList.value = "card";
+        face.classList.value = "face";
+        back.classList.value = "backColors";
 
-      face.src = item.image;
-      card.setAttribute("id", item.id);
+        face.src = item.image;
+        card.setAttribute("id", item.id);
 
-      colorssection.appendChild(card);
-      card.appendChild(face);
-      card.appendChild(back);
+        colorssection.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
 
-      card.addEventListener("click", (e) => {
-        if (!isWaiting) {
-          card.classList.toggle("toggleCard");
-          checkCards(e);
-        }
-      });
-    }
-  }
-
-  //Memory Animals
-  if (animalssection) {
-    const randomizedItems = animals.items.sort(() => Math.random() - 0.5);
-    for (const item of randomizedItems) {
-      const card = document.createElement("div");
-      const face = document.createElement("img");
-      const back = document.createElement("div");
-      card.classList.value = "card";
-      face.classList.value = "face";
-      back.classList.value = "backAnimals";
-
-      face.src = item.image;
-      card.setAttribute("id", item.id);
-
-      animalssection.appendChild(card);
-      card.appendChild(face);
-      card.appendChild(back);
-
-      card.addEventListener("click", (e) => {
-        if (!isWaiting) {
-          card.classList.toggle("toggleCard");
-          checkCards(e);
-        }
-      });
-    }
-  }
-
-  // Memory Verbos  
-  if (verbssection) {
-    const randomizedItems = verbs.items.sort(() => Math.random() - 0.5);
-    for (const item of randomizedItems) {
-      const card = document.createElement("div");
-      const face = document.createElement("img");
-      const back = document.createElement("div");
-      card.classList.value = "card";
-      face.classList.value = "face";
-      back.classList.value = "backVerbs";
-
-      face.src = item.image;
-      card.setAttribute("id", item.id);
-
-      verbssection.appendChild(card);
-      card.appendChild(face);
-      card.appendChild(back);
-
-
-      card.addEventListener("click", (e) => {
-        if (!isWaiting) {
-          card.classList.toggle("toggleCard");
-          checkCards(e);
-        }
-      });
-
-      if (!firstClick) {
-        if (time) {
-          let secs = 0;
-          let mins = 0;
-          let SS
-          let MM
-          setInterval(() => {
-            secs++
-            if (secs == 60) {
-              secs = 0;
-              mins++
-            }
-
-            secs < 10 ? SS = `0${secs}` : SS = `${secs}`
-            mins < 10 ? MM = `0${secs}` : SS = `${mins}`
-
-            document.getElementsByClassName("timer").innerHTML = `$(MM):$(SS)`
-          }, 1000);
-        }
-      }
-    }
-  }
-};
-
-//Voltear cartas
-const checkCards = (e) => {
-  const clickedCard = e.target;
-  clickedCard.classList.add("flipped");
-  const flippedCards = document.querySelectorAll(".flipped");
-
-  //Voltear dos cartas
-  if (flippedCards.length === 2) {
-    isWaiting = true;
-    setTimeout(() => {
-      isWaiting = false;
-      if (
-        flippedCards[0].getAttribute("id") ===
-        flippedCards[1].getAttribute("id")
-      ) {
-        flippedCards.forEach((card) => {
-          card.classList.remove("flipped");
-          card.style.pointerEvents = "none";
-          counter += 50;
-          document.getElementById("score").innerHTML = `Score: ${counter}`;
-          //console.log (counter)
-        });
-      } else {
-        flippedCards.forEach((card) => {
-          card.classList.remove("flipped");
-          card.classList.remove("toggleCard");
+        card.addEventListener("click", (e) => {
+          if (!isWaiting) {
+            card.classList.toggle("toggleCard");
+            checkCards(e);
+          }
         });
       }
+    }
 
-    }, 1000);
-  }
+    //Memory Animals
+    if (animalssection) {
+      const randomizedItems = animals.items.sort(() => Math.random() - 0.5);
+      for (const item of randomizedItems) {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList.value = "card";
+        face.classList.value = "face";
+        back.classList.value = "backAnimals";
 
-  //Timer
+        face.src = item.image;
+        card.setAttribute("id", item.id);
+
+        animalssection.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+        card.addEventListener("click", (e) => {
+          if (!isWaiting) {
+            card.classList.toggle("toggleCard");
+            checkCards(e);
+          }
+        });
+      }
+    }
+
+    // Memory Verbos  
+    if (verbssection) {
+      const randomizedItems = verbs.items.sort(() => Math.random() - 0.5);
+      for (const item of randomizedItems) {
+        const card = document.createElement("div");
+        const face = document.createElement("img");
+        const back = document.createElement("div");
+        card.classList.value = "card";
+        face.classList.value = "face";
+        back.classList.value = "backVerbs";
+
+        face.src = item.image;
+        card.setAttribute("id", item.id);
+
+        verbssection.appendChild(card);
+        card.appendChild(face);
+        card.appendChild(back);
+
+
+        card.addEventListener("click", (e) => {
+          if (!isWaiting) {
+            card.classList.toggle("toggleCard");
+            checkCards(e);
+          }
+
+          if (!firstClick) {
+            time()
+          }
+          firstClick = true;
+        });
+      }
+    }
+
+    };
+
+    //Voltear cartas
+    const checkCards = (e) => {
+      const clickedCard = e.target;
+      clickedCard.classList.add("flipped");
+      const flippedCards = document.querySelectorAll(".flipped");
+
+      //Voltear dos cartas
+      if (flippedCards.length === 2) {
+        isWaiting = true;
+        setTimeout(() => {
+          isWaiting = false;
+          if (
+            flippedCards[0].getAttribute("id") ===
+            flippedCards[1].getAttribute("id")
+          ) {
+            flippedCards.forEach((card) => {
+              card.classList.remove("flipped");
+              card.style.pointerEvents = "none";
+              counter += 50;
+              document.getElementById("score").innerHTML = `Score: ${counter}`;
+              //console.log (counter)
+            });
+          } else {
+            flippedCards.forEach((card) => {
+              card.classList.remove("flipped");
+              card.classList.remove("toggleCard");
+            });
+          }
+
+        }, 1000);
+      }
+
+      //Timer
 
 
 
 
-};
+    };
 
-export default App;
+    if (congratulations) {
+      window.onload = function () {
+        document.getElementById("congratulations").innerHTML =
+          "¡Felicidades " + localStorage.getItem("name") + "!";
+      };
+    }
+
+    /* if (playAgain) {
+      document.getElementById("playAgain").onclick = window.location.reload();
+    } */
+
+    if (nextLevelAnimals) {
+      document.getElementById("nextLevelAnimals").onclick = function () {
+        location.href = "memoryanimals.html";
+      }
+    }
+
+    if (nextLevelVerbs) {
+      document.getElementById("nextLevelVerbs").onclick = function () {
+        location.href = "memoryverbs.html";
+      }
+    }
+
+    if (goToStart) {
+      document.getElementById("goToStart").onclick = function () {
+        location.href = "start.html";
+      }
+    }
+
+    export default App;
